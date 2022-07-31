169eafadf17dbac9b5ff2fe8e786d29abbb7e5d1
Sean Knowles <s@seanknowles.dev>
2022-08-01 00:57:36 +0100

    feat: successfully deployed dev branch using terraform vercel provider

---
+++ apps/terraform/modules/vercel/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+    }
+  }
+}
+
+resource "vercel_project" "example" {
+  name      = "tec2000"
+  framework = "nextjs"
+
+  git_repository = {
+    type = "github"
+    repo = "algoflows/tec2000"
+  }
+
+  #  ignore_command = "npx nx-ignore web"
+}
+
+resource "vercel_project_domain" "domain" {
+  project_id = vercel_project.example.id
+  domain     = "dev-tec2000.vercel.app"
+  git_branch = "dev"
+}
+
+resource "vercel_deployment" "deployment" {
+  project_id       = vercel_project.example.id
+  production       = false
+  ref              = "dev"
+  project_settings = {
+    framework        = "nextjs"
+    install_command  = "npm install"
+    build_command    = "npx nx build web --prod"
+    output_directory = "./dist/apps/web/.next"
+  }
+}
